1a1596bf2993e366432e3a69f0b8bcc00e44d5cf
joshabks <therealjoshbanks@gmail.com>
2025-05-05 22:13:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 user_points = 100
 
-# Get the games for today or tomorrow
+# Get the games for a specific date
 def get_games(date):
     url = f"https://www.balldontlie.io/api/v1/games?start_date={date}&end_date={date}"
     try:
@@ -94,4 +93,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-    
